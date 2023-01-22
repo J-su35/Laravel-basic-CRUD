1a71b779058541098149f4aea0b706a83939d120
J-su35 <det_a248@hotmail.com>
2023-01-22 15:21:50 +0700

    First Commit

---
+++ resources/views/Companies/edit.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Edit Data</title>
+</head>
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Add Company</h2>
+            </div>
+            <div>
+                <a href="{{ route('companies.index') }}" class="btn btn-primary">BACK</a>
+            </div>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{session('status')}}
+                </div>
+            @endif
+            <form action="{{ route('companies.update', $company->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group my-3">
+                        <strong>Company Name</strong>
+                        <input type="text" name="name" value="{{$company->name}}" class="form-control" placeholder="Company Name">
+                        @error('name')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group my-3">
+                        <strong>Company Email</strong>
+                        <input type="text" name="email" value="{{$company->email}}" class="form-control" placeholder="Company Email">
+                        @error('email')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group my-3">
+                        <strong>Company Address</strong>
+                        <input type="text" name="address" value="{{$company->address}}" class="form-control" placeholder="Company Address">
+                        @error('address')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <button type="submit" class="mt-3 btn btn-primary">Update</button>
+                </div>
+                
+            </div>
+            </form>
+        </div>
+</body>
+</html>
